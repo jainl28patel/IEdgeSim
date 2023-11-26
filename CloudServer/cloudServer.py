383c16d7882a6d453fb62ccbb8ed1f2f28baf7bf
Jainil Patel <jainilpatel2003@gmail.com>
2023-11-26 23:03:36 +0530

    final commit: add cloudserver and edgeserver

--- CloudServer/cloudServer.py
+++ CloudServer/cloudServer.py
@@ -25,112 +25,70 @@ processing_delays_size_frac = {
 PERCENT_DELTA_DELAY = 6.9
 PERCENT_DELTA_DATA = 8.9
 
-server_address = ('localhost', 12345)
-
 def handle_mqtt(data,protocol):
     size = sys.getsizeof(data)
-    percent_data_to_process = random.randrange(start=(data_processing_frac[protocol]-PERCENT_DELTA_DATA),
-                                               stop=(data_processing_frac[protocol]+PERCENT_DELTA_DATA))
-    percent_delay = random.randrange(start=(processing_delays_size_frac[protocol]-PERCENT_DELTA_DELAY),
-                                     stop=(processing_delays_size_frac[protocol]+PERCENT_DELTA_DELAY))
+    percent_delay = random.randrange(
+        start=int(processing_delays_size_frac[protocol] - PERCENT_DELTA_DELAY),
+        stop=int(processing_delays_size_frac[protocol] + PERCENT_DELTA_DELAY)
+    )
     
     delay = (size * percent_delay) / 100
-    percent_data_to_send = 100-percent_data_to_process
-    randnum = random.randrange(start=0,stop=100)
-
-    if randnum<percent_data_to_send:
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect(server_address)
-        client_socket.send(base64.encode(json.dumps({'payload':data,'protocol':protocol})))
-        retData = client_socket.recv()
-        client_socket.close()
-
-    time.sleep(delay)
-    return retData
+    print("delay : ", delay)
+    time.sleep(delay/1000)
+    return data
 
 def handle_ampq(data,protocol):
     size = sys.getsizeof(data)
-    percent_data_to_process = random.randrange(start=(data_processing_frac[protocol]-PERCENT_DELTA_DATA),
-                                               stop=(data_processing_frac[protocol]+PERCENT_DELTA_DATA))
-    percent_delay = random.randrange(start=(processing_delays_size_frac[protocol]-PERCENT_DELTA_DELAY),
-                                     stop=(processing_delays_size_frac[protocol]+PERCENT_DELTA_DELAY))
-    
+    percent_delay = random.randrange(
+        start=int(processing_delays_size_frac[protocol] - PERCENT_DELTA_DELAY),
+        stop=int(processing_delays_size_frac[protocol] + PERCENT_DELTA_DELAY)
+    )
+
     delay = (size * percent_delay) / 100
-    percent_data_to_send = 100-percent_data_to_process
-    randnum = random.randrange(start=0,stop=100)
-
-    if randnum<percent_data_to_send:
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect(server_address)
-        client_socket.send(base64.encode(json.dumps({'payload':data,'protocol':protocol})))
-        retData = client_socket.recv()
-        client_socket.close()
-
-    time.sleep(delay)
-    return retData
+    time.sleep(delay/1000)
+    return data
 
 def handle_coap(data,protocol):
     size = sys.getsizeof(data)
-    percent_data_to_process = random.randrange(start=(data_processing_frac[protocol]-PERCENT_DELTA_DATA),
-                                               stop=(data_processing_frac[protocol]+PERCENT_DELTA_DATA))
-    percent_delay = random.randrange(start=(processing_delays_size_frac[protocol]-PERCENT_DELTA_DELAY),
-                                     stop=(processing_delays_size_frac[protocol]+PERCENT_DELTA_DELAY))
-    
+    percent_delay = random.randrange(
+        start=int(processing_delays_size_frac[protocol] - PERCENT_DELTA_DELAY),
+        stop=int(processing_delays_size_frac[protocol] + PERCENT_DELTA_DELAY)
+    )
+
     delay = (size * percent_delay) / 100
-    percent_data_to_send = 100-percent_data_to_process
-    randnum = random.randrange(start=0,stop=100)
-
-    if randnum<percent_data_to_send:
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect(server_address)
-        client_socket.send(base64.encode(json.dumps({'payload':data,'protocol':protocol})))
-        retData = client_socket.recv()
-        client_socket.close()
-
-    time.sleep(delay)
-    return retData
+    time.sleep(delay/1000)
+    return data
 
 def handle_zigbee(data,protocol):
     size = sys.getsizeof(data)
-    percent_data_to_process = random.randrange(start=(data_processing_frac[protocol]-PERCENT_DELTA_DATA),
-                                               stop=(data_processing_frac[protocol]+PERCENT_DELTA_DATA))
-    percent_delay = random.randrange(start=(processing_delays_size_frac[protocol]-PERCENT_DELTA_DELAY),
-                                     stop=(processing_delays_size_frac[protocol]+PERCENT_DELTA_DELAY))
+    percent_delay = random.randrange(
+        start=int(processing_delays_size_frac[protocol] - PERCENT_DELTA_DELAY),
+        stop=int(processing_delays_size_frac[protocol] + PERCENT_DELTA_DELAY)
+    )
     
     delay = (size * percent_delay) / 100
-    percent_data_to_send = 100-percent_data_to_process
-    randnum = random.randrange(start=0,stop=100)
-
-    if randnum<percent_data_to_send:
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect(server_address)
-        client_socket.send(base64.encode(json.dumps({'payload':data,'protocol':protocol})))
-        retData = client_socket.recv()
-        client_socket.close()
-
-    time.sleep(delay)
-    return retData
+    time.sleep(delay/1000)
+    return data
 
 def handle_client(clientsocket, address):
-    while True:
-        data = clientsocket.recv(1024)
-        msg = json.loads(base64.decode(data))
-        retData = None
+    data = clientsocket.recv(1024)
+    msg = json.loads(base64.b64decode(data).decode())
 
-        if msg['protocol'] == 'mqtt':
-            retData = handle_mqtt(msg['payload'],msg['protocol'])
-        elif msg['protocol'] == 'ampq':
-            retData = handle_ampq(msg['payload'],msg['protocol'])
-        elif msg['protocol'] == 'coap':
-            retData = handle_coap(msg['payload'],msg['protocol'])
-        elif msg['protocol'] == 'zigbee':
-            retData = handle_zigbee(msg['payload'],msg['protocol'])
+    if msg['protocol'] == 'mqtt':
+        handle_mqtt(msg['payload'],msg['protocol'])
+    elif msg['protocol'] == 'ampq':
+        handle_ampq(msg['payload'],msg['protocol'])
+    elif msg['protocol'] == 'coap':
+        handle_coap(msg['payload'],msg['protocol'])
+    elif msg['protocol'] == 'zigbee':
+        handle_zigbee(msg['payload'],msg['protocol'])
 
-        clientsocket.send(retData)
+    clientsocket.send(data)
+    clientsocket.close()
 
-def main():
+if __name__ == "__main__":
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((socket.gethostname(), 8000))
+    s.bind(('127.0.0.1', 9000))
     s.listen(10)
     thread_list = []
 
@@ -145,6 +103,3 @@ def main():
         for thread in thread_list:
             if not thread.is_alive():
                 thread_list.remove(thread)
-
-if __name__ == "__main__":
-    main()
