55583bdaba17cf4c62432518aee39cfaabc0139c
DarthRevan07 <t_bakshi@cs.iitr.ac.in>
2023-11-26 21:45:18 +0530

    Final CoAP implementation

--- CoAP/coap_client.py
+++ CoAP/coap_client.py
@@ -12,10 +12,18 @@ class EdgeCoAPCache:
             timestamp, data = self.cache[uri]
             if time.time() - timestamp < 60:
                 return data
-        return None
+        else:
+            return None
 
     def update_cache(self, uri, data):
         self.cache[uri] = (time.time(), data)
+    def delete_from_cache(self, uri):
+        if uri in self.cache:
+            del self.cache[uri]
+
+    def show_cache(self):
+        return dict(self.cache)
+
 
 class CoAPClientWithCaching:
     def __init__(self, server_address, server_port, edge_cache):
@@ -28,21 +36,22 @@ class CoAPClientWithCaching:
         return random.randint(0, 65535)
 
     def _build_coap_message(self, method, uri, payload=None):
+        # Setting first 2 bits to 1 --> Corresponding to Version
+        # Setting Message-LAYER field  {type} to NON --> 01 (Next 2 bits)
+        # Token length to 0 --> 0000 in the last 4 bits
+        header = bytearray([0x40 | (1 << 4) | 0x00])
 
-        if (method == "GET"):
-            # Creating the Initial Header construct
-            header = bytearray([0x40 | (1 << 4) | 0x00])  # CoAP Header
-            # Setting first 2 bits to 1 --> Corresponding to Version
-            # Setting Message type to GET --> 01 (Next 2 bits)
-            # Token length to 0 --> 0000 in the last 4 bits
-        elif(method == "POST"):
-            header = bytearray([0x40 | (2 << 4) | 0x00])
-        elif(method == "PUT"):
-            header = bytearray([0x40 | (3 << 4) | 0x00])
-        elif(method == "DELETE"):
-            header = bytearray([0x40 | (0 << 4) | 0x00])
-        else:
-            raise ValueError(f"Invalid CoAP method : {method}")
+        method_codes = {
+            'GET': 1,
+            'POST': 2,
+            'PUT': 3,
+            'DELETE': 4
+        }
+        if method.upper() not in method_codes:
+            raise ValueError("Invalid CoAP method code")
+
+        code_value = method_codes[method.upper()]
+        header.extend(struct.pack('B', code_value))   #Pack the Request/Response Layer code to a 1 byte field
 
         message_id = self._generate_message_id()
         header.extend(struct.pack('!H', message_id))  # Message ID (16 bits)
@@ -53,6 +62,62 @@ class CoAPClientWithCaching:
             header.extend(payload.encode())  # Payload
 
         return bytes(header)
+
+    def _parse_coap_response(self, data):
+        # Extract CoAP header information
+        # Extracting the first byte to get the version, type, and token length info.
+        version_type_token_length = data[0]
+        version = (version_type_token_length >> 6) & 0b11
+        # Version encoded in the first 2 bits
+        type_code = (version_type_token_length >> 4) & 0b0011
+        # Type code in the next 2 bits
+        #       print(int(type_code))
+
+        # The next 1 byte is the request/response layer code ->
+        method_code_value = struct.unpack('B', data[1:2])[0]
+        # The next 2 bytes contain the message ID, which need to be unpacked from the Big-endian binary format it was sent in
+        message_id = struct.unpack('!H', data[2:4])[0]
+
+        # So, our URI starts from the 3rd index, or the 4th byte; and extends till we encounter our Payload Marker
+        uri_start = 4
+        while data[uri_start] != 0xFF:
+            uri_start += 1
+
+        # Extract URI and payload
+        uri = data[4:data.index(0xFF)].decode('utf-8')
+        payload = data[data.index(0xFF) + 1:].decode('utf-8')
+        return version, int(type_code), int(method_code_value), message_id, uri, payload
+
+    def _receive_response(self):
+        try:
+            response, server_address = self.client_socket.recvfrom(1024)
+            version, type_code, method_code_value, message_id, uri, payload = self._parse_coap_response(response)
+
+            if method_code_value == 65:  # Response code 2.01 (Created)
+                self.edge_cache.update_cache(uri, payload)
+                return None
+
+            elif method_code_value == 68:  # Response code 2.04 (Changed)
+                self.edge_cache.update_cache(uri, payload)
+                return None
+
+            elif method_code_value == 69:  # Response code 2.05 (Content)
+                self.edge_cache.update_cache(uri, payload)
+                return None
+
+            elif method_code_value == 66:  #Response code 2.02 (Deleted)
+                self.edge_cache.delete_from_cache(uri)
+                return None
+
+            elif method_code_value == 132:  #Response code 4.04 (Not Found)
+                return None
+
+            elif method_code_value == 133:  #Response code 4.05 (Method Not Allowed)
+                return None
+
+        except Exception as e:
+            print(f"Error receiving CoAP response: {e}")
+            return None
 
 
 #Method to send GET request -->
@@ -68,29 +133,30 @@ class CoAPClientWithCaching:
         try:
             message = self._build_coap_message("GET", uri)
             self.client_socket.sendto(message, (self.server_address, self.server_port))
-            response, server_address = self.client_socket.recvfrom(1024)
-
-            self.edge_cache.update_cache(uri, response)
+            self._receive_response()
+#            response, server_address = self.client_socket.recvfrom(1024)
+#            self.edge_cache.update_cache(uri, response)
             #response.decode('utf-8')
-            print(f"Received response from {server_address}: {response}")
-            return response
-            # print(f"Received response from {server_address}: {response.decode('utf-8')}")
-            # return response.decode('utf-8')
+#            print(f"Received message : {uri}: {message}")
+            print("Cache after GET operation:")
+            print(self.edge_cache.show_cache())
+            return None
+
         except Exception as e:
             print(f"Error sending/receiving CoAP GET request: {e}")
 #        finally:
 #           self.client_socket.close()
 
-    def get_post_request(self, uri, payload):
+    def send_post_request(self, uri, payload):
         try:
             message = self._build_coap_message("POST", uri, payload)
             self.client_socket.sendto(message, (self.server_address, self.server_port))
-            response, server_address = self.client_socket.recvfrom(1024)
-
-            self.edge_cache.update_cache(uri, response)
-            print(f"Received response from {server_address}: {response}")
-            return response
-
+            self._receive_response()
+#            response, server_address = self.client_socket.recvfrom(1024)
+#            self.edge_cache.update_cache(uri, response)
+            print("Cache after POST operation:")
+            print(self.edge_cache.show_cache())
+            return None
         except Exception as e:
             print(f"Error sending/receiving CoAP POST request: {e}")
 
@@ -99,11 +165,13 @@ class CoAPClientWithCaching:
         try:
             message = self._build_coap_message("PUT", uri, payload)
             self.client_socket.sendto(message, (self.server_address, self.server_port))
-            response, server_address = self.client_socket.recvfrom(1024)
-
-            self.edge_cache.update_cache(uri, response)
-            print(f"Received response from {server_address}: {response}")
-            return response
+#           response, server_address = self.client_socket.recvfrom(1024)
+            self._receive_response()
+#            self.edge_cache.update_cache(uri, response)
+#             print(f"Received response from {server_address}: {response}")
+#             return response
+            print("Cache after PUT operation:")
+            print(self.edge_cache.show_cache())
 
         except Exception as e:
             print(f"Error sending/receiving CoAP PUT request: {e}")
@@ -112,11 +180,14 @@ class CoAPClientWithCaching:
         try:
             message = self._build_coap_message("DELETE", uri)
             self.client_socket.sendto(message, (self.server_address, self.server_port))
-            response, server_address = self.client_socket.recvfrom(1024)
+            self._receive_response()
 
-            self.edge_cache.update_cache(uri, response)
-            print(f"Received response from {server_address}: {response}")
-            return response
+#            self.edge_cache.update_cache(uri, response)
+#            print(f"Received response from {server_address}: {response}")
+#            return response
+            print("Cache after DELETE operation:")
+            print(self.edge_cache.show_cache())
+            return None
 
         except Exception as e:
             print(f"Error sending/receiving CoAP DELETE request: {e}")
@@ -125,8 +196,8 @@ class CoAPClientWithCaching:
 if __name__ == "__main__":
     edge_cache = EdgeCoAPCache()
     coap_client = CoAPClientWithCaching("localhost", 5683, edge_cache)  # Adjust server address and port
-    for i in range(5):
-        coap_client.send_get_request("/hello")
+    # for i in range(5):
+    #     coap_client.send_get_request("/hello")
 
     # Example: Send POST request
     coap_client.send_post_request("/roof_sensor", "25.61728, 12.412, 29.111, 81.1920")
@@ -134,6 +205,6 @@ if __name__ == "__main__":
     coap_client.send_post_request("/front_sensor", "32.81292, 52.531, 18.019, 07.888")
     # Example: Send PUT request
     coap_client.send_put_request("/wall_sensor", "32.81292, 52.531, 18.019, 07.888")
-
+    coap_client.send_get_request("/back_sensor")
     # Example: Send DELETE request
     coap_client.send_delete_request("/telemetry")
--- CoAP/coap_server.py
+++ CoAP/coap_server.py
@@ -27,9 +27,10 @@ class ThreadSafeResourceRepository:
 
 
 class CoAPServer:
-    def __init__(self, bind_address, bind_port):
+    def __init__(self, bind_address, bind_port, resource_repository):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.server_socket.bind((bind_address, bind_port))
+        self.resource_repository = resource_repository
         print(f"CoAP server listening on {bind_address}:{bind_port}")
 
     def _parse_coap_message(self, data):
@@ -41,24 +42,31 @@ class CoAPServer:
         type_code = (version_type_token_length >> 4) & 0b0011
         #Type code in the next 2 bits
 #       print(int(type_code))
+
+        #The next 1 byte is the request/response layer code ->
+        method_code_value = struct.unpack('B', data[1:2])[0]
         #The next 2 bytes contain the message ID, which need to be unpacked from the Big-endian binary format it was sent in
-        message_id = struct.unpack('!H', data[1:3])[0]
+        message_id = struct.unpack('!H', data[2:4])[0]
 
         #So, our URI starts from the 3rd index, or the 4th byte; and extends till we encounter our Payload Marker
-        uri_start = 3
+        uri_start = 4
         while data[uri_start] != 0xFF:
             uri_start += 1
 
         # Extract URI and payload
-        uri = data[3:data.index(0xFF)].decode('utf-8')
+        uri = data[4:data.index(0xFF)].decode('utf-8')
         payload = data[data.index(0xFF) + 1:].decode('utf-8')
-        return version, int(type_code), message_id, uri, payload
+        return version, int(type_code), int(method_code_value), message_id, uri, payload
 
-    def _build_coap_response(self, message_id, response_code, payload=None):
+    def _build_coap_response(self, response_code, message_id, uri, payload=None):
         # CoAP Header: Ver(2 bits), Type(2 bits), Token Length(4 bits)
-        header = bytearray([0x40 | (1 << 4) | (response_code >> 5)])
+        header = bytearray([0x40 | (1 << 4) | 0x00])
+        #Response Code (8 bits)
+        header.extend(struct.pack('B', response_code))
         # Message ID (16 bits)
         header.extend(struct.pack('!H', message_id))
+
+        header.extend(uri.encode())
         # Payload Marker
         header.append(0xFF)
 
@@ -69,53 +77,67 @@ class CoAPServer:
         return bytes(header)
 
     def handle_request(self, data, client_address):
-        version, type_code, message_id, uri, payload = self._parse_coap_message(data)
-
+        version, type_code, method_code_value, message_id, uri, payload = self._parse_coap_message(data)
         # Process the CoAP request as needed
         print(f"Received CoAP {self._get_type_name(int(type_code))} request from {client_address}:")
+        print(f"  The request is for : {self._get_method_name(method_code_value)}")
         print(f"  Message ID: {message_id}")
         print(f"  URI: {uri}")
         print(f"  Payload: {payload}")
 
+        response_payload = None
         # Process the CoAP request and prepare the response
-        response_code = 200  # OK by default
+        response_code = (2 << 5) | 3   # VALID by default
 
-        if type_code == 1:  # GET request
+        if method_code_value == 1:  # GET request
             resource_data = self.resource_repository.get_resource(uri)
             if resource_data is not None:
                 response_payload = resource_data
+                response_code = (2 << 5) | 5  # Represents 2.05 (69) ==> Content
             else:
-                response_code = 404  # Not Found
-                response_payload = "Resource not found."
-        elif type_code == 2:  # POST request
+                response_code = (4 << 5) | 4  # Not Found
+#                response_payload = "Resource not found."
+
+        elif method_code_value == 2:  # POST request
             self.resource_repository.update_resource(uri, payload)
-            response_payload = "Resource created/updated."
-        elif type_code == 3:  # PUT request
+#            response_payload = "Resource created/updated."
+            response_code = (2 << 5) | 1        #Represents 2.01 (65)==> Created
+
+        elif method_code_value == 3:  # PUT request
             resource_data = self.resource_repository.get_resource(uri)
             if resource_data is not None:
                 self.resource_repository.update_resource(uri, payload)
-                response_payload = "Resource updated."
+#                response_payload = "Resource updated."
+                response_code = (2 << 5) | 4    #Represents 2.04 ==> Changed
             else:
-                response_code = 404  # Not Found
-                response_payload = "Resource not found."
-        elif type_code == 0:  # DELETE request
+                response_code = (4 << 5) | 4    #Represents 4.04 ==> Not Found
+#               response_payload = "Resource not found."
+
+        elif method_code_value == 4:  # DELETE request
             resource_data = self.resource_repository.get_resource(uri)
             if resource_data is not None:
                 self.resource_repository.delete_resource(uri)
-                response_payload = "Resource deleted."
+#                response_payload = "Resource deleted."
+                response_code = (2 << 5) | 2  #Represents 2.02 ==> Deleted
             else:
-                response_code = 404  # Not Found
-                response_payload = "Resource not found."
+                response_code = (4 << 5) | 4    #Represents 4.04 ==> Not Found
+#               response_payload = "Resource not found."
         else:
-            response_code = 405  # Method Not Allowed
-            response_payload = "Method not allowed for this resource."
+            response_code = (4 << 5) | 5   #Represents 4.05 => Method Not allowed
+#            response_payload = "Method not allowed for this resource."
+        # Print the contents of the resource repository
+        print("Contents of Resource Repository:")
+        for key, value in self.resource_repository.get_all_resources().items():
+            print(f"URI: {key}, Data: {value}")
 
-        response = self._build_coap_response(message_id, response_code, response_payload)
+        response = self._build_coap_response(response_code, message_id, uri, response_payload)
         self.server_socket.sendto(response, client_address)
 
-
+    def _get_method_name(self, method_code_value):
+        req_type = {1: "GET", 2: "POST", 3: "PUT", 4: "DELETE"}
+        return req_type.get(method_code_value, "Unknown")
     def _get_type_name(self, type_code):
-        types = {0: "DELETE", 1: "GET", 2: "POST", 3: "PUT"}
+        types = {0: "CON", 1: "NON", 2: "ACK", 3: "RST"}
         return types.get(type_code, "Unknown")
 
     def run(self):
@@ -128,5 +150,6 @@ class CoAPServer:
             print("\nCoAP server stopped.")
 
 if __name__ == "__main__":
-    coap_server = CoAPServer("localhost", 5683)
+    resource_repository = ThreadSafeResourceRepository()
+    coap_server = CoAPServer("localhost", 5683, resource_repository)
     coap_server.run()
